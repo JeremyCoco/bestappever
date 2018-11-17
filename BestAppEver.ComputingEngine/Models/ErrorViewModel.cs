436d3b52030c616dc7b800d6c0d4705a92af2fde
Bartosz Bernaś <leukocyty@gmail.com>
2018-11-17 01:09:14 +0100

    adding ultra super duper .net core solution! Heck yeah!

---
+++ BestAppEver.ComputingEngine/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace BestAppEver.ComputingEngine.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
